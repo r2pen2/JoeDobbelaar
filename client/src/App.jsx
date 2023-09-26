9dba751a83c128bae921afb0df5b33632e0934c0
r2pen2 <joedobbelaar@gmail.com>
2023-09-26 14:42:06 -0400

    AppContextProvider in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -59,21 +59,23 @@ export function App(props) {
   // Return the app
   return (
     <div className="App d-flex flex-column align-items-center w-100" data-testid="app">
-      { isTestingEnvironment && <meta data-testid="wl-testing-flag" /> }
-      <Router>
-        <div className="app-content">
-            <section className="d-flex flex-column align-items-center justify-content-center" style={{height: "100vh", width: "100vw"}}>
-              <img src={powerBrick} alt="power-brick" data-testid="lego-brick" />
-              <Text h1 data-testid="title-text">BP-10700</Text>
-            </section>
-          {/** Place Navigation Here */}
-            <Routes>
-              {/** Place Routes Here */}
-            </Routes>
-          {/** Place Footer Here */}
-          <LineButton />
-        </div>
-      </Router>
+      <AppContextProvider>
+        { isTestingEnvironment && <meta data-testid="wl-testing-flag" /> }
+        <Router>
+          <div className="app-content">
+              <section className="d-flex flex-column align-items-center justify-content-center" style={{height: "100vh", width: "100vw"}}>
+                <img src={powerBrick} alt="power-brick" data-testid="lego-brick" />
+                <Text h1 data-testid="title-text">BP-10700</Text>
+              </section>
+            {/** Place Navigation Here */}
+              <Routes>
+                {/** Place Routes Here */}
+              </Routes>
+            {/** Place Footer Here */}
+            <LineButton />
+          </div>
+        </Router>
+      </AppContextProvider>
     </div>
   );
 }
