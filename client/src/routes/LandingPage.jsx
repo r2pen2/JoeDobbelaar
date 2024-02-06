d4ec5c7ad088c5b1374be4bdd699348dc4765c73
Joe Dobbelaar <joedobbelaar@gmail.com>
2024-02-06 13:52:09 -0500

    Not printable on press

--- client/src/routes/LandingPage.jsx
+++ client/src/routes/LandingPage.jsx
@@ -152,7 +152,7 @@ const Splash = ({setSitesModalOpen, setConfettiLeft, confettiLeft, setPrintable,
       <Button color="gradient" size="lg" className="hover-scale" onClick={() => window.open("/resume", "_blank")} onMouseEnter={() => {setGrayscaleGlitter(false); setConfettiLeft(confettiLeft + dConfetti)}}>
         Download Resume
       </Button>
-      <Link href="/resume?light=true" target='_blank' onMouseEnter={() => {setConfettiLeft(confettiLeft + dConfetti); setPrintable(true); setGrayscaleGlitter(true)}} onMouseLeave={() => setPrintable(false)} onPress={window.location.reload}>
+      <Link href="/resume?light=true" target='_blank' onMouseEnter={() => {setConfettiLeft(confettiLeft + dConfetti); setPrintable(true); setGrayscaleGlitter(true)}} onMouseLeave={() => setPrintable(false)} onPress={() => setPrintable(false)}>
         <Text style={{fontSize:"1rem", color:"#ffffff66"}} className="mt-2">
           Printable Version
         </Text>
