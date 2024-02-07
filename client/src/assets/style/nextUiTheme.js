3c676248e2052079f1420ef1b2c9c6db7c35bb9d
r2pen2 <joedobbelaar@gmail.com>
2024-02-06 20:30:11 -0500

    Update nextUiTheme and Web-Legos subproject commit

--- client/src/assets/style/nextUiTheme.js
+++ client/src/assets/style/nextUiTheme.js
@@ -3,7 +3,7 @@ import { createTheme } from "@nextui-org/react";
 
 /** Default NextUI compatible theme object (lightmode only for now) */
 export const nextUiTheme = createTheme({
-  type: "light", // it could be "light" or "dark"
+  type: "dark", // it could be "light" or "dark"
   theme: {
     colors: {
       primaryLight: "$purple200",
